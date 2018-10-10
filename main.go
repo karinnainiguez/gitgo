8281c5059b7235a5747f5761ed63a81d438eb58c
Karinna Iniguez <kngz@amazon.com>
2018-10-10 10:33:01 -0700

    Add user file with functions to call

--- main.go
+++ main.go
@@ -18,17 +18,30 @@ func main() {
 
 	// if user does not supply flags:
 	if flag.NFlag() == 0 {
-		fmt.Printf("Usage: %s [options]\n", os.Args[0])
-		fmt.Println("Options:")
-		flag.PrintDefaults()
-		os.Exit(1)
+		printUsage()
 	}
 
 	users := strings.Split(user, ",")
 	fmt.Printf("Searching user(s): %s\n", users)
 
+	for _, u := range users {
+		result := getUsers(u)
+		fmt.Printf("Username: %v\n", result.Login)
+		fmt.Printf("Name: %v\n", result.Name)
+		fmt.Printf("Email: %v\n", result.Email)
+		fmt.Printf("Bio: %v\n", result.Bio)
+		fmt.Printf("")
+
+	}
 }
 
 func init() {
 	flag.StringVarP(&user, "user", "u", "", "Search Users")
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [options]\n", os.Args[0])
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+	os.Exit(1)
 }
